7c53af0830651f7d1a6373f4e4a14055d258011c
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2024-01-20 13:39:31 +0100

    fs-extra - readFile

--- src/storage/file/readFile/readFile.sandbox.ts
+++ src/storage/file/readFile/readFile.sandbox.ts
@@ -2,8 +2,9 @@ import readFileString from './readFileString';
 import doer from 'utils/monad/either/do/doer';
 import { WORK_DIR } from 'src/defaults';
 import * as sniff from 'supersniff';
+import { FSReadTextOptions } from '../file.types';
 
-const DEFAUTL_ENCODING: string = 'utf8';
+const DEFAUTL_ENCODING: FSReadTextOptions = 'utf8';
 const FILE_PATH: string = `${WORK_DIR}src/storage/file/readFile/readFile.ts`;
 
 readFileString(FILE_PATH, DEFAUTL_ENCODING)
