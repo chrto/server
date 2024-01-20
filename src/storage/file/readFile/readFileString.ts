4a41ac86a280ffcc0834a9c8354e887f1175a231
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2024-01-20 13:39:31 +0100

    fs-extra - readFile

--- src/storage/file/readFile/readFileString.ts
+++ src/storage/file/readFile/readFileString.ts
@@ -1,5 +1,6 @@
 import readFileUnbound from './readFile.unbound';
 import { readFile } from 'fs-extra';
 import callback from '../common/callback/callback';
+import { FSReadTextOptions } from '../file.types';
 
-export default readFileUnbound<string, string>(readFile, callback);
+export default readFileUnbound<string, FSReadTextOptions>(readFile, callback);
