da5df814bda04c6646ffa70a250771977d4ac619
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2024-01-20 14:27:16 +0100

    fs-extra - writeFile

--- src/storage/file/writeFile/writeFile.sandbox.ts
+++ src/storage/file/writeFile/writeFile.sandbox.ts
@@ -4,8 +4,7 @@ import { WORK_DIR } from 'src/defaults';
 import * as sniff from 'supersniff';
 import { FSWriteOptions } from '../file.types';
 
-const DEFAUTL_ENCODING: string = 'utf8';
-const OPTIONS: FSWriteOptions = { encoding: DEFAUTL_ENCODING };
+const OPTIONS: FSWriteOptions = { encoding: 'utf8' };
 const DATA: any = JSON.stringify(OPTIONS);
 const FILE_PATH: string = `${WORK_DIR}src/storage/file/writeFile/remove_me.json`;
 
