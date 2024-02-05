f218027219d7136c4ff1b91b1a7f99dca33057d7
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2024-02-05 12:29:55 +0100

    test without chai assertions

--- src/common/httpErrors.test.ts
+++ src/common/httpErrors.test.ts
@@ -1,4 +1,3 @@
-import {assert} from 'chai';
 import { AppError } from './error';
 
 import {
@@ -14,50 +13,50 @@ describe('Common/Types tests', () => {
   it('NotFound extends Error', () => {
     let err = new NotFound('Not Found');
 
-    assert.instanceOf(err, NotFound);
-    assert.equal(err.message, 'Not Found');
-    assert.equal(err.code, 'not.found');
+    expect(err).toBeInstanceOf(NotFound)
+    expect(err.message).toBe('Not Found');
+    expect(err.code).toBe('not.found');
   });
 
   it('InvalidInput extends Error', () => {
     let err = new InvalidInput('Invalid Input');
 
-    assert.instanceOf(err, InvalidInput);
-    assert.equal(err.message, 'Invalid Input');
-    assert.equal(err.code, 'invalid.input');
+    expect(err).toBeInstanceOf(InvalidInput);
+    expect(err.message).toBe('Invalid Input');
+    expect(err.code).toBe('invalid.input');
   });
 
   it('BadRequest extends Error', () => {
     let err = new BadRequest('Invalid Input');
 
-    assert.instanceOf(err, BadRequest);
-    assert.equal(err.message, 'Invalid Input');
-    assert.equal(err.code, 'bad.request');
+    expect(err).toBeInstanceOf(BadRequest);
+    expect(err.message).toBe('Invalid Input');
+    expect(err.code).toBe('bad.request');
   });
 
   it('NotAuthorized extends Error', () => {
     let err = new NotAuthorized('Not Authorized');
 
-    assert.instanceOf(err, NotAuthorized);
-    assert.instanceOf(err, AppError);
-    assert.instanceOf(err, Error);
-    assert.equal(err.message, 'Not Authorized');
-    assert.equal(err.code, 'not.authorized');
+    expect(err).toBeInstanceOf(NotAuthorized);
+    expect(err).toBeInstanceOf(AppError);
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe('Not Authorized');
+    expect(err.code).toBe('not.authorized');
   });
 
   it('NotAuthenticated extends Error', () => {
     let err = new NotAuthenticated('Not Authenticated');
 
-    assert.instanceOf(err, NotAuthenticated);
-    assert.equal(err.message, 'Not Authenticated');
-    assert.equal(err.code, 'not.authenticated');
+    expect(err).toBeInstanceOf(NotAuthenticated);
+    expect(err.message).toBe('Not Authenticated');
+    expect(err.code).toBe('not.authenticated');
   });
 
   it('Conflict extends Error', () => {
     let err = new Conflict('Conflict');
 
-    assert.instanceOf(err, Conflict);
-    assert.equal(err.message, 'Conflict');
-    assert.equal(err.code, 'conflict');
+    expect(err).toBeInstanceOf(Conflict);
+    expect(err.message).toBe('Conflict');
+    expect(err.code).toBe('conflict');
   });
 });
